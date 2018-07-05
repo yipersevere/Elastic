1eacfe9f7d76be465b9053708747de280fb29c4d
yizhouper <yipersevere@outlook.com>
2018-07-05 14:14:10 +0300

    add

--- elastic/pytorch_code/models/__init__.py
+++ elastic/pytorch_code/models/__init__.py
@@ -1,5 +1,6 @@
+# num_outputs = 1 # initially only one classifier output
 from .Elastic_ResNet_Others import Elastic_ResNet18, Elastic_ResNet34, Elastic_ResNet101
-from .Elastic_ResNet import Elastic_ResNet50, Elastic_ResNet34
+from .Elastic_ResNet import Elastic_ResNet
 from .Elastic_Inception import Elastic_InceptionV3
 from .Elastic_DenseNet import Elastic_DenseNet121
 from .Elastic_MobileNet import Elastic_MobileNet
