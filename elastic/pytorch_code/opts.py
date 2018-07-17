11652278493fe66e61afa1bd852c7517ce251d6b
yizhouper <yipersevere@outlook.com>
2018-07-17 22:08:54 +0300

    forget to track

--- elastic/pytorch_code/opts.py
+++ elastic/pytorch_code/opts.py
@@ -5,10 +5,10 @@ parser = argparse.ArgumentParser(description='PyTorch Elastic-ResNet implementat
 
 parser.add_argument('--data', type=str, help="training and testing data, data=cifar10; data=cifar100", default="cifar100")
 parser.add_argument('--num_classes', type=int, help="classification number, 10 or 100", default=100)
-parser.add_argument('--target_size', type=tuple, help='default target size is (224,224,3)', default=(229,229,3))
-parser.add_argument('--epochs', type=int, help="epoch number, default 1, set 100 or 1000", default=1)
+parser.add_argument('--target_size', type=tuple, help='default target size is (224,224,3)', default=(224,224,3))
+parser.add_argument('--epochs', type=int, help="epoch number, default 1, set 100 or 1000", default=3)
 parser.add_argument('--dropout_rate', type=float, help="dropout rate, (default: 0.2)", default=0.2)
-parser.add_argument('--batch_size', type=int, help="batch size for training and testing, (default: 16)", default=64)
+parser.add_argument('--batch_size', type=int, help="batch size for training and testing, (default: 16)", default=1)
 parser.add_argument('--learning_rate', type=float, help="initial learning rate (default: 1e-3)", default=1e-2)
 parser.add_argument('--pretrain_learning_rate', type=float, help="initial learning rate (default: 1e-3)", default=1e-3)
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
@@ -26,8 +26,8 @@ parser.add_argument('--layers_weight_change', type=int, default=0,
 # parser.add_argument('--model', type=str, help="model folder, like ElasticNN-ResNet50", default="Elastic_ResNet")
 # parser.add_argument('--model_name', type=str, help="exact model name", default="CIFAR10_all_intermediate_resblock_Elastic_ResNet50")
 
-parser.add_argument('--model', type=str, help="model folder, like ElasticNN-ResNet50", default="Elastic_InceptionV3")
-parser.add_argument('--model_name', type=str, help="exact model name", default="pytorch_CIFAR100_1_intermediate_classifiers_Elastic_InceptionV3_no_imagenet")
+parser.add_argument('--model', type=str, help="model folder, like ElasticNN-ResNet50", default="Elastic_SqueezeNet")
+parser.add_argument('--model_name', type=str, help="exact model name", default="pytorch_CIFAR100_all_intermediate_classifiers_Elastic_SqueezeNet")
 
 parser.add_argument('--manual-seed', default=0, type=int, metavar='N',
                     help='Manual seed (default: 0)')
