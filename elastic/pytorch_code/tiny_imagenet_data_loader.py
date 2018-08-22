7385df81903132d19df7187a537773e5510a5ce0
yizhouper <yipersevere@outlook.com>
2018-08-22 19:23:26 +0300

    add a new way to do backpropagation

--- elastic/pytorch_code/tiny_imagenet_data_loader.py
+++ elastic/pytorch_code/tiny_imagenet_data_loader.py
@@ -11,16 +11,14 @@ import shutil
 import os
 import argparse
 import cv2
-
+from torch.utils.data.sampler import WeightedRandomSampler
 # Create an ArgumentParser object which will obtain arguments from command line
 
 # tut thinkstation
 # global data
 
 
-def tiny_image_data_loader(data):
-
-
+def tiny_image_data_loader(data, args):
 
     def create_val_folder(data):
         """
@@ -88,11 +86,8 @@ def tiny_image_data_loader(data):
 
         return tiny_class_dict
 
-    # data = "/media/yi/e7036176-287c-4b18-9609-9811b8e33769/tiny_imagenet/tiny-imagenet-200"
-
     # Batch Sizes for dataloaders
-    train_batch_size = 16 # total 500*200 images, 1000 batches of 100 images each
-    validation_batch_size = 16  # total 10000 images, 10 batches of 1000 images each
+    train_batch_size = validation_batch_size = 16  # total 10000 images, 10 batches of 1000 images each
 
     train_root = os.path.join(data, 'train')  # this is path to training images folder
     validation_root = os.path.join(data, 'val/images')  # this is path to validation images folder
@@ -111,24 +106,65 @@ def tiny_image_data_loader(data):
                                             transform=transforms.Compose([transforms.Resize((224, 224)),
                                                                             transforms.ToTensor(),
                                                                             normalize]))
-    # Create training dataloader
-    train_data_loader = torch.utils.data.DataLoader(train_data, batch_size=train_batch_size, shuffle=True,
-                                                            num_workers=5)
-    # Create validation dataloader
-    validation_data_loader = torch.utils.data.DataLoader(validation_data,
-                                                                batch_size=validation_batch_size,
-                                                                shuffle=False, num_workers=5)
+
+    if args.debug:
+        # weights for tiny-imagenet, 
+        weights = [1] * 200
+        # weights = [len(data)for data, label in train_data]
+        
+        train_sampler = WeightedRandomSampler(weights,num_samples=5000,replacement=True)
+        test_sampler = WeightedRandomSampler(weights,num_samples=1000,replacement=True)
+
+
+        # # see trorch/utils/data/sampler.py
+        # class DummySampler(Sampler):
+        #     def __init__(self, data):
+        #         self.num_samples = len(data)
+
+        #     def __iter__(self):
+        #         print ('\tcalling Sampler:__iter__')
+        #         return iter(range(self.num_samples))
+
+        #     def __len__(self):
+        #         print ('\tcalling Sampler:__len__')
+        #         return self.num_samples
+
+        # my_sampler = DummySampler(train_data)
+
+        print("enter debug mode, load subset of train data")
+        # train_data.train_data=train_data.train_data[:5000]
+        # train_data.train_labels=train_data.train_labels[:5000]
+
+        print("enter debug mode, load subset of test data")
+        # validation_data.test_data=validation_data.test_data[:1000]
+        # validation_data.test_labels=validation_data.test_labels[:1000]
+
+        # Create training dataloader
+        train_data_loader = torch.utils.data.DataLoader(train_data, batch_size=train_batch_size, sampler=train_sampler, shuffle=False,
+                                                                num_workers=5)
+        # Create validation dataloader
+        validation_data_loader = torch.utils.data.DataLoader(validation_data, batch_size=validation_batch_size, sampler=test_sampler,
+                                                                    shuffle=False, num_workers=5)
+
+    else:
+
+
+        # Create training dataloader
+        train_data_loader = torch.utils.data.DataLoader(train_data, batch_size=train_batch_size, shuffle=True,
+                                                                num_workers=5)
+        # Create validation dataloader
+        validation_data_loader = torch.utils.data.DataLoader(validation_data,
+                                                                    batch_size=validation_batch_size,
+                                                                    shuffle=False, num_workers=5)
 
     # list of class names, each class name is the name of the parent folder of the images of that class
     class_names = train_data.classes
     num_classes = len(class_names)
 
     # tiny_class = {'n01443537': 'goldfish', 'n01629819': 'European fire salamander', 'n01641577': 'bullfrog', ...}
-    tiny_class = class_extractor(class_names, data)  # create dict of label string for each of 200 classes
-
-    return train_data_loader, validation_data_loader, tiny_class
+    # tiny_class = class_extractor(class_names, data)  # create dict of label string for each of 200 classes
 
 
-# if __name__ == "__main__":
-#     train_data_loader, validation_data_loader, tiny_class = tiny_image_data_loader()
-#     print("done")
+
+
+    return train_data_loader, validation_data_loader
